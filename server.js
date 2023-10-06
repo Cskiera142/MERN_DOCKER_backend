bfb812b838387414b0f8ec9437424c5befcb4af1
Colton Skiera <cskiera142@gmail.com>
2023-10-06 06:04:55 -0600

    MongoDB setup

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
 
 app.use(logger);
 
